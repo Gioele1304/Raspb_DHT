ec3000ecd9506de799797f015932e73531457214
Gioele Valzolgher <gioele.valzolgher@marconirovereto.it>
2025-03-25 16:15:35 +0100

    feat:si

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 import time
 import board
 import adafruit_dht
+import requests
 
 # Cablaggio del sensore DHT
 sensor = adafruit_dht.DHT11(board.D4)
 
-while True:
+# Nostra chiave API da usare per le richieste
+key = "F79HHSQCZEMGJGM5"
 
-# acquisizione del sensore
+# Intestazioni necessarie alla richiesta POST
+headers = {
+    "Content-type": "application/x-www-form-urlencoded",
+    "Accept": "text/plain",
+}
+
+thingspeak_url = "https://api.thingspeak.com/update"
+
+while True:
     try:
+        # acquisizione del sensore
         temperature = sensor.temperature
         humidity = sensor.humidity
 
-        #stampa dei valori letti, formattati con max 1 cifre decimale
+        # stampa dei valori letti, formattati con max 1 cifre decimale
         print("Temp={0:0.1f}ºC, Humidity={1:0.1f}%".format(temperature, humidity))
+
+        # Invio della richiesta POST temperatura
+        params = {"field1": temperature, "field2": humidity, "key": key}
+        r = requests.post(thingspeak_url, data=params, headers=headers)
+
+        # Debug: stampa a video del risultato della richiesta
+        print("Dato inviato: Temp={0:0.1f}ºC, Humidity={1:0.1f}%".format(temperature, humidity))
+        print(r)
 
     except RuntimeError as error:
         print(error.args[0])
@@ -24,4 +43,5 @@ while True:
         sensor.exit()
         raise error
 
-    time.sleep(2)
+    # Wait 5 seconds before fetching data again
+    time.sleep(5)
